a69bf1b743903dd46fd11667b9f432d2c8883852
Maya Shibuki <shibuki@everqueue.com>
2014-10-14 03:47:01 +0000

    何故か全部更新

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,2 +1,10 @@
 class Area < ActiveRecord::Base
+  belongs_to :prefecture, foreign_key: :prefecture_id
+
+  def prefecture_str
+    if self.prefecture
+      self.prefecture.name
+    end
+  end
+
 end
--- app/models/spring_info.rb
+++ app/models/spring_info.rb
@@ -1,2 +1,24 @@
 class SpringInfo < ActiveRecord::Base
+  belongs_to :area, foreign_key: :area_id
+  belongs_to :category, foreign_key: :category_id
+  belongs_to :osmotic, foreign_key: :osmotic_id
+
+  def area_str
+    if self.area
+      self.area.name
+    end
+  end
+
+  def category_str
+    if self.category
+      self.category.name
+    end
+  end
+
+  def osmotic_str
+    if self.osmotic
+      self.osmotic.name
+    end
+  end
+
 end
